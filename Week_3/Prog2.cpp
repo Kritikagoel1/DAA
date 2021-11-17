b45216a47f956cb2b37fac5431b9d768deb242cd
Kritikagoel1 <89133242+Kritikagoel1@users.noreply.github.com>
2021-11-17 19:26:52 +0530

    Update Prog2.cpp

--- Week_3/Prog2.cpp
+++ Week_3/Prog2.cpp
@@ -1,38 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
-int insertionSort(int A[], int size)
+ 
+void swap(int *xp, int *yp)
 {
-   int i, key, j,c=0;
-   for(i = 1; i < size; i++)
-   {
-       key = A[i];
-       j = i - 1;
-       while (j >= 0 && A[j] > key)
-       { c++;
-           A[j + 1] = A[j];
-           j = j - 1;
-       }
-       A[j + 1] = key;
-   }
-   return c;
+	
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+ 
+int selectionSort(int arr[], int n)
+{
+    int i,c=0, j, min_idx;
+ 
+    for (i = 0; i < n-1; i++)
+    {
+        min_idx = i;
+        for (j = i+1; j < n; j++)
+        if (arr[j] < arr[min_idx]){
+        	c++;
+            min_idx = j;
+        }
+        swap(&arr[min_idx], &arr[i]);
+    }
+    return c;
 }
 void printArray(int arr[], int size)
 {
-    for (int i = 0; i < size; i++)
+    int i;
+    for (i=0; i < size; i++)
         cout << arr[i] << " ";
     cout << endl;
 }
 int main()
 {
-    int i,len,x, arr[10];
+   int i,len,x, arr[10];
      cin>>len;
     for(i=0;i<len;i++){
     	cin>>arr[i];
 	}
-    int c= insertionSort(arr, len);
- 
+	int c = selectionSort(arr, len);
     printArray(arr, len);
-    cout<<"comparisons = "<<c;
- 
+     cout<<"comparisons = "<<c;
     return 0;
 }
